85b478fa19f171bb36c8a1ffa857bc496f42eb49
Nicholas Mehrle <nmehrle@gmail.com>
2020-04-28 00:02:49 -0400

    updated getLCFeatures

--- getLCFeatures.py
+++ getLCFeatures.py
@@ -3,6 +3,22 @@ import sys, os
 import numpy as np
 from scipy.optimize import curve_fit
 import h5py
+import pickle
+
+def type_of_script():
+  try:
+    ipy_str = str(type(get_ipython()))
+    if 'zmqshell' in ipy_str:
+      return 'jupyter'
+    if 'terminal' in ipy_str:
+      return 'ipython'
+  except:
+    return 'terminal'
+
+if type_of_script() == 'jupyter':
+  from tqdm import tqdm_notebook as tqdm
+else:
+  from tqdm import tqdm
 
 def getApKey(apnum):
   return "Aperture_%.3d" % apnum
@@ -12,8 +28,10 @@ def closest_idx(array, value):
   idx = (np.abs(array - value)).argmin()
   return idx
 
-def trapezoid(x, depth, t0, total_duration, full_duration):
-  y = np.zeros(len(x))
+def trapezoid(x, depth, t0, total_duration, full_duration, baseline):
+  total_duration = np.maximum(1, total_duration)
+  full_duration  = np.maximum(1, full_duration)
+  y = np.ones(len(x)) * baseline
   t1 = t0 - total_duration/2.0
   t2 = t0 - full_duration/2.0
   t3 = t0 + full_duration/2.0
@@ -23,28 +41,26 @@ def trapezoid(x, depth, t0, total_duration, full_duration):
   slope = -depth * 2 / (total_duration - full_duration)
   ingress = (x-t1) * -slope
   egress  = (x-t3) * slope + depth
-  y[t_idx[0]:t_idx[1]] = np.minimum(0,ingress[t_idx[0]:t_idx[1]])
-  y[t_idx[1]:t_idx[2]] = depth
-  y[t_idx[2]:t_idx[3]] = np.maximum(depth,egress[t_idx[2]:t_idx[3]])
+  y[t_idx[0]:t_idx[1]] = np.minimum(0,ingress[t_idx[0]:t_idx[1]]) + baseline
+  y[t_idx[1]:t_idx[2]] = depth + baseline
+  y[t_idx[2]:t_idx[3]] = np.maximum(depth,egress[t_idx[2]:t_idx[3]]) + baseline
   
   return y
 
-def fitTrap(data, view='local'):
+def fitTrap(data, errors=None, view='local'):
   x = np.arange(len(data))
 
-  if view == 'local':
-    guess = [np.min(data), len(data)/2, len(data)/3, max(2,len(data)/3 - 10)]
-  elif view == 'global':
-    guess = [np.min(data), len(data)/2, len(data)/10, max(2,len(data)/10 - 10)]
+  if view == 'global':
+    guess = [np.min(data), len(data)/2, len(data)/10, max(2,len(data)/10 - 10), 0]
   else:
-    guess = [np.min(data), len(data)/2, len(data)/3, max(2,len(data)/3 - 10)]
+    guess = [np.min(data), len(data)/2, len(data)/3, max(2,len(data)/3 - 10), 0]
 
   popt,_ = curve_fit(trapezoid, x, data, guess)
 
   return x, popt
 
 def getDepth(data, view='local'):
-  x, p = fitTrap(data, view)
+  x, p = fitTrap(data, view=view)
   depth = p[0]
   error = np.std(data - trapezoid(x,*p))
 
@@ -124,6 +140,39 @@ def processLCFile(lcfilename):
   lcfile.close()
   return np.array(data)
 
+def sortOutApertures(group, bestap):
+  n = np.shape(group[getApKey(bestap)])[0]
+  organizedData = np.zeros((3, n))
+
+  if bestap == 0:
+    organizedData[0] = group[getApKey(0)]
+    organizedData[1] = group[getApKey(1)]
+    organizedData[2] = group[getApKey(2)]
+  elif bestap == 4:
+    organizedData[0] = group[getApKey(2)]
+    organizedData[1] = group[getApKey(3)]
+    organizedData[2] = group[getApKey(4)]
+  else:
+    organizedData[0] = group[getApKey(bestap - 1)]
+    organizedData[1] = group[getApKey(bestap    )]
+    organizedData[2] = group[getApKey(bestap + 1)]
+
+  return organizedData
+
+def quickLCFILE(lcfilename):
+  data = h5py.File(lcfilename, 'r')
+  ret = {}
+
+  bestap = data['bestap'][0]
+
+  ret['LocalView'] = sortOutApertures(data['LocalView'], bestap)
+  ret['GlobalView'] = sortOutApertures(data['GlobalView'], bestap)
+
+  ret['LocalDepths'] = np.array([getDepth(ret['LocalView'][i], 'local')[0] for i in range(3)])
+  ret['GlobalDepths'] = np.array([getDepth(ret['GlobalView'][i], 'global')[0] for i in range(3)])
+
+  return ret
+
 def loadPlanetLabels(dataPath, labelsFile):
   labels_tsv = np.genfromtxt(os.path.join(dataPath,labelsFile), delimiter="\t",skip_header=3,usecols=(0),dtype="i8,S5",names=["id"])
   planetLabels = []
@@ -132,28 +181,32 @@ def loadPlanetLabels(dataPath, labelsFile):
 
   return planetLabels
 
-def processSector(sectorPath, processedTICIDs, planetLabels):
+def processSector(dataDir, sector, planetLabels, processFN=processLCFile,
+  subpath='preprocessed/'
+):
   nsuccess = 0
-  nomit   = 0
-  sector_data = []
+  nfail = 0
   failed_tics = []
+  sector_data = []
 
-  for i,lcfilename in enumerate(os.listdir(sectorPath)):
-    print("{} / {}\r".format(i, len(os.listdir(sectorPath))-1), end="")
-    # skip if already processed
-    if lcfilename in processedTICIDs:
-      continue
-    # skip if not lc file
-    if lcfilename.split('.')[-1] != 'h5':
-      continue
+  fulldatapath = os.path.join(dataDir, sector, subpath)
+  if not os.path.exists(fulldatapath):
+    print("{} data does not exist.".format(sector))
+    return
 
-    ticID = lcfilename.split('.')[0]
+  for i, lcfilename in enumerate(tqdm(os.listdir(fulldatapath), desc=sector)):
+    try:
+      ticID, extension = lcfilename.split('.')
+      if extension != 'h5':
+        continue
+    except ValueError:
+      continue
 
     try:
-      lc_data = processLCFile(os.path.join(sectorPath,lcfilename))
+      lc_data = processFN(os.path.join(fulldatapath,lcfilename))
     except Exception as e:
       failed_tics.append(ticID)
-      nomit+=1
+      nfail+=1
       continue
 
     if ticID in planetLabels:
@@ -161,42 +214,81 @@ def processSector(sectorPath, processedTICIDs, planetLabels):
     else:
       label = -1
 
-    lc_data = np.append(lc_data, label)
+    try:
+      lc_data['label'] = label
+    except (IndexError, TypeError):
+      lc_data = np.append(lc_data, label)
 
     sector_data.append(lc_data)
-    processedTICIDs.append(lcfilename)
     nsuccess+=1
+  return sector_data, failed_tics, nsuccess, nfail
 
-  return np.array(sector_data), processedTICIDs, nsuccess, nomit, failed_tics
+def processAllSectors(sectors, labelsFile, dataDir,
+  processFN=processLCFile,
+  subpath='preprocessed', output='lcFeatures'
+):
+  planetLabels = loadPlanetLabels(dataDir, labelsFile)
 
-def processAllSectors(sectors, labelsFile, dataPath, subpath='preprocessed', output='lcFeatures'):
-  processedTICIDs = []
-  planetLabels = loadPlanetLabels(dataPath, labelsFile)
-  failedTICIDs = []
-
-  for i in range(len(sectors)-1,-1,-1):
-    sector = sectors[i]
-
-    print(sector, end='')
-    print('  ----')
-
-    sectorPath = os.path.join(dataPath,sector, subpath)
-    if not os.path.exists(sectorPath):
-      print("{} data does not exist.".format(sector))
+  for sector in sectors:
+    try:
+      sector_data, failed_tics, nsuccess, nfail = processSector(dataDir, sector,planetLabels, processFN, subpath)
+    except TypeError:
       continue
 
-    sector_data, processedTICIDs, nsuccess, nomit, failed_tics = processSector(sectorPath, processedTICIDs, planetLabels)
-
-    print('Loaded {:4d} files from {}'.format(nsuccess, sectorPath))
-    print('Omitting {} files.'.format(nomit))
+    print('Loaded {:4d} files from {}'.format(nsuccess, sector))
+    print('Omitting {} files.'.format(nfail))
     print('')
     print('')
 
-    for each in failed_tics:
-      failedTICIDs.append(each)
+    with open(os.path.join(dataDir, sector, output+'.pickle'), 'wb') as f:
+      pickle.dump(sector_data, f)
+    with open(os.path.join(dataDir, sector, 'TICSFailedFeatures.pickle'),'wb') as f:
+      pickle.dump(failed_tics,f)
 
-    np.save(os.path.join(dataPath, sector, output), sector_data)
-    np.save(os.path.join(dataPath, 'TICSFailedFeatures'),failedTICIDs)
+def removeDuplicates(dataPath, sectors,
+  subpath='preprocessed/', duplicatePath='duplicates/'
+):
+  # Sort sectors in descending order
+  sector_nums = [int(sector.split('-')[1]) for sector in sectors]
+  sorted_idx = np.argsort(sector_nums)
+  sorted_sectors = list(np.array(sectors)[sorted_idx])[::-1]
+
+  foundTics    = []
+  foundSectors = []
+
+  fileCount = 0
+  duplicateCount = 0
+
+  for sector in tqdm(sorted_sectors):
+    # Path to lightcurves
+    lcPath = os.path.join(dataPath, sector, subpath)
+
+    # Path to duplicate Lightcurves
+    duplicatePath = os.path.join(lcPath, 'duplicates/')
+    if not os.path.exists(duplicatePath):
+      os.mkdir(duplicatePath)
+
+    for lcfile in os.listdir(lcPath):
+      # get TIC ID from lightcurve files
+      if '.h5' not in lcfile:
+        continue
+      tic = lcfile.split('.')[0]
+
+      if tic in foundTics:
+        # This TIC ID has been found in a later sector
+        # Move the lightcurve file to the duplicate folder
+        idx = np.where(np.array(foundTics) == tic)[0][0]
+        srcName = os.path.join(lcPath,lcfile)
+        destName = os.path.join(duplicatePath, f'{tic}_{foundSectors[idx]}.h5')
+        os.rename(srcName, destName)
+        duplicateCount+=1
+      else:
+        # New TIC ID, log that it was found
+        foundTics.append(tic)
+        foundSectors.append(sector)
+      fileCount+=1
+
+  print(f'{fileCount} files found, {duplicateCount} duplicates, {fileCount-duplicateCount} originals')
 
 # # for blender
 # dataPath = 'Data/'
